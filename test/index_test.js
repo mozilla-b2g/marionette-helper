bcc09f3aa9acfa7485e6e80b79d1ccee6660fa0d
Michael Henretty <michael.henretty@gmail.com>
2013-11-21 17:55:19 -0800

    Bug 941942 - make waitForElement allow for non DOM element, and add waitForElementToDisappear

--- test/index_test.js
+++ test/index_test.js
@@ -6,7 +6,14 @@ suite('MarionetteHelper', function() {
 
   setup(function(done) {
     subject = client.helper;
-    setTimeout(done, 2500);  // In the stead of using the BootWatcher.
+    client.setSearchTimeout(2000);
+    setTimeout(function() {
+      client.executeScript(function() {
+        // start with a clean slate
+        document.body.innerHTML = '';
+      });
+      done();
+    }, 2500); // In the stead of using the BootWatcher.
   });
 
   test('#wait', function() {
@@ -16,7 +23,6 @@ suite('MarionetteHelper', function() {
     assert.ok(after - before >= 1000);
   });
 
-
   test('#waitFor', function(done) {
     var i = 0;
     subject.waitFor(function() {
@@ -25,21 +31,95 @@ suite('MarionetteHelper', function() {
     }, function() {
       assert.strictEqual(i, 5);
       done();
-    }, 50, 1000);
+    }, 50, 200);
   });
 
-  test('#waitForElement exists', function(done) {
-    subject.waitForElement('#screen');
-    done();
+  test('#waitForElement, element added to DOM', function() {
+    var myRandomID = 'YAAAAAAYRandomWow' + Date.now();
+    client.executeScript(function(myRandomID) {
+      setTimeout(function() {
+        var el = document.createElement('div');
+        el.id = myRandomID;
+        el.innerHTML = 'Hello Test!';
+        document.body.appendChild(el);
+      }, 500);
+    }, [myRandomID]);
+    subject.waitForElement('#' + myRandomID);
   });
 
-  test('#waitForElement does not exist', function(done) {
-    try {
-      subject.waitForElement('#not-so-much-there');
-    } catch (e) {
-      assert.strictEqual(e.type, 'NoSuchElement');
-      done();
-    }
+  test('#waitForElement, element displayed with CSS', function() {
+    var myRandomID = 'YAAAAAAYRandomWow' + Date.now();
+    client.executeScript(function(myRandomID) {
+      var el = document.createElement('div');
+      el.id = myRandomID;
+      el.innerHTML = 'Hello Test!';
+      el.style.display = 'none';
+      document.body.appendChild(el);
+      setTimeout(function() {
+        el.style.display = 'block';
+      }, 500);
+    }, [myRandomID]);
+    subject.waitForElement('#' + myRandomID);
+  });
+
+  test('#waitForElementToDisappear DOM with an Element', function() {
+    var myRandomID = 'YAAAAAAYRandomWow' + Date.now();
+    client.executeScript(function(myRandomID) {
+      var el = document.createElement('div');
+      el.id = myRandomID;
+      el.innerHTML = 'Hello Test!';
+      document.body.appendChild(el);
+      setTimeout(function() {
+        document.body.removeChild(el);
+      }, 500);
+    }, [myRandomID]);
+    var el = client.findElement('#' + myRandomID);
+    subject.waitForElementToDisappear(el);
+  });
+
+  test('#waitForElementToDisappear DOM with a string', function() {
+    var myRandomID = 'YAAAAAAYRandomWow' + Date.now();
+    client.executeScript(function(myRandomID) {
+      var el = document.createElement('div');
+      el.id = myRandomID;
+      el.innerHTML = 'Hello Test!';
+      document.body.appendChild(el);
+      setTimeout(function() {
+        document.body.removeChild(el);
+      }, 500);
+    }, [myRandomID]);
+    var el = client.findElement('#' + myRandomID);
+    subject.waitForElementToDisappear('#' + myRandomID);
+  });
+
+  test('#waitForElementToDisappear CSS with an Element', function() {
+    var myRandomID = 'YAAAAAAYRandomWow' + Date.now();
+    client.executeScript(function(myRandomID) {
+      var el = document.createElement('div');
+      el.id = myRandomID;
+      el.innerHTML = 'Hello Test!';
+      document.body.appendChild(el);
+      setTimeout(function() {
+        el.style.display = 'none';
+      }, 500);
+    }, [myRandomID]);
+    var el = client.findElement('#' + myRandomID);
+    subject.waitForElementToDisappear(el);
+  });
+
+  test('#waitForElementToDisappear DOM with a string', function() {
+    var myRandomID = 'YAAAAAAYRandomWow' + Date.now();
+    client.executeScript(function(myRandomID) {
+      var el = document.createElement('div');
+      el.id = myRandomID;
+      el.innerHTML = 'Hello Test!';
+      document.body.appendChild(el);
+      setTimeout(function() {
+        el.style.display = 'none';
+      }, 500);
+    }, [myRandomID]);
+    var el = client.findElement('#' + myRandomID);
+    subject.waitForElementToDisappear('#' + myRandomID);
   });
 
   suite.skip('#waitForAlert', function() {
